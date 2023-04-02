0dbe8fe60a448f272b5075c1d542fdfe8e6d268b
codethi <docencia.thiago@gmail.com>
2023-04-02 18:47:57 -0300

    chore: setup database

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express, { json } from "express";
 import cors from "cors";
+import ConnectToMongoDb from "database";
 
 const app = express();
 app.use(json());
 app.use(cors());
+ConnectToMongoDb.execute();
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running in port: ${port}`));
