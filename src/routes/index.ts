aec2190394bd4c14b6e71a38315f78abdaa5ef4e
codethi <docencia.thiago@gmail.com>
2023-04-07 18:16:13 -0300

    remove product and category

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,10 @@
 import { Router } from "express";
 import authRouter from "./Auth/authRoutes";
 import userRoutes from "./Users/userRoutes";
-import categoryRouter from "./Category/categoryRoutes";
-import productRouter from "./Product/productRoutes";
 
 const router = Router();
 
 router.use("/users", userRoutes);
 router.use("/auth", authRouter);
-router.use("/categories", categoryRouter);
-router.use("/products", productRouter);
 
 export default router;
