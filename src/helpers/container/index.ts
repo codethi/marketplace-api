14b27c0c575e0c34d082b14fdc8e1c2c8a2dfd8f
codethi <docencia.thiago@gmail.com>
2023-04-07 21:55:44 -0300

    refact

--- src/helpers/container/index.ts
+++ src/helpers/container/index.ts
@@ -1,14 +1,19 @@
+import { container } from "tsyringe";
+
 import { UserRepositoriesMongoDb } from "../../modules/Users/repositories/implementations/UserRepositoriesMongoDb";
 import { IUserRepositories } from "../../modules/Users/repositories/IUserRepositories";
-import { container } from "tsyringe";
-import { IAuthRepositories } from "modules/Auth/repositpries/IAuthRepositories";
-import { AuthRepositoriesMongoDb } from "modules/Auth/repositpries/implementations/AuthRepositoriesMongoDb";
-import { ICategoriesRepository } from "modules/Categories/repositories/ICategoriesRepository";
-import { CategoriesRepository } from "modules/Categories/repositories/implementations/CategoriesRepository";
-import { IProductRepository } from "modules/Products/repositories/IProductRepository";
-import { ProductRepository } from "modules/Products/repositories/implementations/ProductRepository";
-import { IOrderRepository } from "modules/Orders/repositories/IOrderRepositories";
-import { OrderRepositoriesMongoDb } from "modules/Orders/repositories/implementations/OrderRepositoriesMongoDb";
+
+import { AuthRepositoriesMongoDb } from "../../modules/Auth/repositpries/implementations/AuthRepositoriesMongoDb";
+import { IAuthRepositories } from "../../modules/Auth/repositpries/IAuthRepositories";
+
+import { CategoriesRepository } from "../../modules/Categories/repositories/implementations/CategoriesRepository";
+import { ICategoriesRepository } from "../../modules/Categories/repositories/ICategoriesRepository";
+
+import { ProductRepository } from "../../modules/Products/repositories/implementations/ProductRepository";
+import { IProductRepository } from "../../modules/Products/repositories/IProductRepository";
+
+import { OrderRepositoriesMongoDb } from "../../modules/Orders/repositories/implementations/OrderRepositoriesMongoDb";
+import { IOrderRepository } from "../../modules/Orders/repositories/IOrderRepositories";
 
 container.registerSingleton<IUserRepositories>(
   "UserRepositories",
--- src/modules/Orders/useCases/createOrder/createOrderService.ts
+++ src/modules/Orders/useCases/createOrder/createOrderService.ts
@@ -1,7 +1,5 @@
-import { NotFoundError } from "helpers/errors/apiErrors";
-import { Order } from "modules/Orders/entities/Order";
-import { IOrderRepository } from "modules/Orders/repositories/IOrderRepositories";
-import { IUserRepositories } from "modules/Users/repositories/IUserRepositories";
+import { Order } from "../../entities/Order";
+import { IOrderRepository } from "../../repositories/IOrderRepositories";
 import { ObjectId } from "mongodb";
 import { inject, injectable } from "tsyringe";
 
@@ -12,7 +10,8 @@ export class CreateOrderService {
     private orderRepository: IOrderRepository
   ) {}
 
-  async execute(_id: ObjectId, data: Order): Promise<void> {
+  async execute(_id: ObjectId, data: Order): Promise<void> 
+  {
     const order = { ...data, user_id: _id };
     await this.orderRepository.create(order);
   }
