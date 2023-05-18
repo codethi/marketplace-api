60f17975029ef028f278dfae392c1044eee9a0d2
codethi <docencia.thiago@gmail.com>
2023-05-18 14:09:07 -0300

    feat: carts

---
+++ src/modules/Carts/useCases/createCart/createCartController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { CreateCartService } from "./createCartService";
+
+class CreateCartController {
+  async handle(req: Request, res: Response): Promise<Response | undefined> {
+    const { _id } = res.locals.user;
+    const body = req.body;
+    const creatCartService = container.resolve(CreateCartService);
+    await creatCartService.execute(_id, body);
+    return res.sendStatus(201);
+  }
+}
+
+export default new CreateCartController();
